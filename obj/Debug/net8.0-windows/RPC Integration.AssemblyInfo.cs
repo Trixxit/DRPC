6a5c64421b529af75654778c0878fdcf1e68a3c8
Nexus-Vediir <justaworkemail1414@gmail.com>
2024-10-18 23:33:12 +1100

    Removed uneeded files

--- obj/Debug/net8.0-windows/RPC Integration.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/RPC Integration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPC Integration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c6cbf1bd731abfef6e015a726cee1e59d50d1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519a10ddc64206e302f1237fafdde7f798e33914")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPC Integration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPC Integration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
